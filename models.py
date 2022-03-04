4e9cf88b40180280910fd6ea761e4bb6d060a550
Engines-screaming <karl.ravago@gmail.com>
2022-03-04 11:36:58 -0800

    updated db design

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -12,39 +13,68 @@ class User(db.Model):
     __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
     name = db.Column(db.Text, nullable=False)
+    
     email = db.Column(db.Text, unique=True)
+    
     state = db.Column(db.Text, nullable=False)
+    
     password = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.Datetime, nullable=False)
-    updated_at = db.Column(db.Datetime, nullable=False)
+
+    created_at = db.Column(db.Datetime, 
+                            default=datetime.datetime.now, 
+                            onupdate=datetime.datetime.now, 
+                            nullable=False)
+
+    updated_at = db.Column(db.Datetime, 
+                            default=datetime.datetime.now, 
+                            onupdate=datetime.datetime.now, 
+                            nullable=False)
 
 
 class Resorts(db.Model):
     __tablename__ = "resorts"
-
+    
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    
     name = db.Column(db.Text, nullable=False)
+    
     lat = db.Column(db.Float, nullable=False)
+    
     lon = db.Column(db.Float, nullable=False)
-    created_at = db.Column(db.Datetime, nullable=False)
-    updated_at = db.Column(db.Datetime, nullable=False)
+    
+    created_at = db.Column(db.Datetime, 
+                            default=datetime.datetime.now, 
+                            onupdate=datetime.datetime.now, 
+                            nullable=False)
+
+    updated_at = db.Column(db.Datetime, 
+                            default=datetime.datetime.now, 
+                            onupdate=datetime.datetime.now, 
+                            nullable=False)
 
 
 class Favorites(db.Model):
     __tablename__ = "favorites"
-
+    
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
     resort_id = db.Column(db.Integer, db.ForeignKey('resorts.id'))
-    created_at = db.Column(db.Datetime, nullable=False)
-    updated_at = db.Column(db.Datetime, nullable=False)
+    
+    timestamp = db.Column(db.DateTime, default=datetime.datetime.now, nullable=False)
 
+    
 
 class History(db.Model):
     __tablename__ = "history"
-
+    
     id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.DateTime, nullable=False)
+    
+    timestamp = db.Column(db.DateTime, default=datetime.datetime.now, nullable=False)
+    
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
     resort_id = db.Column(db.Integer, db.ForeignKey('resorts.id'))
