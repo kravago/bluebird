3ce2fc23419f3beb92d3ef1b3a9d6918b3aab9e9
Engines-screaming <karl.ravago@gmail.com>
2022-03-08 23:32:38 -0800

    form validates emails

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms import StringField, PasswordField, EmailField
+from wtforms.validators import InputRequired, Email
 
 
 class RegisterForm(FlaskForm):
     name = StringField("Name", validators=[InputRequired()])
-    email = StringField("Email", validators=[InputRequired()])
+    email = EmailField("Email", validators=[InputRequired(), Email()])
     username = StringField("Username", validators=[InputRequired()])
     state = StringField("State", validators=[InputRequired()])
     password = PasswordField("Password", validators=[InputRequired()])
